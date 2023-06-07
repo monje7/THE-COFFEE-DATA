19412f9c03a011e68b5d1b16444fdabcd3f530fb
Danna947 <dannalopez072004@gmail.com>
2023-06-07 11:17:50 -0500

    validacion

---
+++ js/validacion.js
@@ -0,0 +1,47 @@
+let txtNombre = document.getElementById("txtNombre");
+let txtApellidos = document.getElementById("txtApellidos");
+let txtCorreo = document.getElementById("txtCorreo");
+let password = document.getElementById("password");
+let selectType = document.getElementById("selectType");
+let btnRegistrar = document.getElementById("btnRegistrar");
+let btnMistake = document.getElementById("btnRegistrar");
+let hiddenAlert = document.getElementById("hiddenAlert");
+let hiddenBreack = document.getElementById("hiddenBreack");
+let form = document.getElementById("form");
+btnRegistrar.addEventListener("click", function(){
+    if (txtNombre.value.length === 0){
+        alert("El nombre del usuario, campo obligatorio")
+        hiddenBreack.style.visibility = "visible";
+        hiddenBreack.style.opacity = "1";
+        
+    }
+    else if(txtApellidos.value.length === 0){
+        alert("Campo obligatorio para registrarse!");
+
+    }
+    else if(txtCorreo.value.length === 0){
+        alert("Campo obligatorio para registrarse!");
+
+    }
+    else if(password.value.length === 0){
+        alert("Campo obligatorio para registrarse!");
+
+    }
+    else if(selectType.value.length === 0){
+        alert("Campo obligatorio para registrarse! Por favor complete la información");
+
+    }
+    else{
+        // alert("Bienvenido");
+        hiddenBreack.style.visibility = "visible";
+        hiddenBreack.style.opacity = "1";
+        btnMistake.addEventListener("click", function(){
+            form.submit()
+        })
+
+    }
+});
+btnMistake.addEventListener("click", function(){
+    hiddenBreack.style.visibility = "hidden";
+    hiddenBreack.style.opacity= "0";
+})
